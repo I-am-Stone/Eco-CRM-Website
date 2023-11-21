59785e54737e4e01d5ee37e437303e112e105c53
I-am-stone <dilipchaulgain86@gmail.com>
2023-11-21 21:17:06 +0545

    Design

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -29,14 +29,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # local Application
+    "inventory.apps.InventoryConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # external application
     "tailwind",
     "theme",
+    "django_browser_reload",
 
 ]
 
@@ -48,6 +52,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = "ecommerce.urls"
@@ -124,3 +129,4 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
+TAILWIND_CSS_PATH = 'css/dist/styles.css'
