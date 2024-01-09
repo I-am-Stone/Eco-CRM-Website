c40cc119f098a1254ae1340a01b13725ce127f21
I-am-stone <dilipchaulgain86@gmail.com>
2024-01-09 11:36:28 +0545

    part 3

--- user/models.py
+++ user/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class user(models.Model):
+    name = models.CharField(
+        max_length=255,
+        unique=True,
+        null=False,
+        blank=False,
+        verbose_name=_("brand name"),
+        help_text=_("format: required, unique, max-255"),
+    )
