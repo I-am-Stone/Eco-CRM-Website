603a97d7a74f1be848270e894910ce8386ccebf1
I-am-stone <dilipchaulgain86@gmail.com>
2024-01-26 13:52:01 +0545

    rework

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -129,3 +129,7 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'inventory/static'),
+]
