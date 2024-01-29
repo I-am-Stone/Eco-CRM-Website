61655cd735e4d3ae834bab927cd8c21c54a47f5f
I-am-Stone <dilipchaulagain86@gmail.com>
2024-01-29 16:47:12 +0545

    new-part

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATIC_ROOT = '/home/stone/Website/inventory/static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
@@ -130,6 +130,3 @@ INTERNAL_IPS = [
 ]
 NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'inventory/static'),
-]
