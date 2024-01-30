d4dfb3c3b95d68e2586fdc89aebe9e49c693dfc4
I-am-Stone <dilipchaulagain86@gmail.com>
2024-01-30 16:34:45 +0545

    user-checkout

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # local Application
     "inventory.apps.InventoryConfig",
+    "user.apps.UserConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
