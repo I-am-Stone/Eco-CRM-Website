33b660326fac281d68cafa2c04161f361637dd8c
I-am-Stone <dilipchaulagain86@gmail.com>
2024-04-15 17:09:39 +0545

    inprogress cart

--- inventory/static/inventory/js/main.js
+++ inventory/static/inventory/js/main.js
@@ -11,3 +11,23 @@ function openDrawer(){
 function closeDrawer(){
     document.getElementById('myDrawer').classList.toggle('active')
 }
+
+
+
+let cart_item = [];
+
+function addToCart(productName, price, image) {
+    const item ={
+        name:productName,
+        price:price,
+        image:image
+    };
+    
+    cart_item.push(item)
+
+    updateCartDisplay();
+}
+
+function updateCartDisplay() {
+    
+}
