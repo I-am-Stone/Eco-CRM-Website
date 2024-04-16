b01412eab7aea28ff9ed8b83981c557a31dc0f37
I-am-Stone <dilipchaulagain86@gmail.com>
2024-04-16 16:44:20 +0545

    on going cart funcionality

--- inventory/static/inventory/js/main.js
+++ inventory/static/inventory/js/main.js
@@ -16,18 +16,49 @@ function closeDrawer(){
 
 let cart_item = [];
 
-function addToCart(productName, price, image) {
-    const item ={
-        name:productName,
-        price:price,
-        image:image
+function addToCart(productName, price, ImageURL) {
+    const item = {
+        name: productName,
+        price: price,
+        image:ImageURL
     };
     
-    cart_item.push(item)
+    cart_item.push(item);
 
     updateCartDisplay();
 }
 
 function updateCartDisplay() {
-    
+    const cartList = document.querySelector('.cart-list');
+
+    cartList.innerHTML = '';
+
+    cart_item.forEach((item, index) => {
+        const listItem = document.createElement('li');
+
+        const img = document.createElement('img');
+        img.src = item.image
+        img.alt = item.name
+        img.width = '10px'
+
+        const itemInfo = document.createElement('span');
+
+        itemInfo.textContent = `${item.name} - $${item.price.toFixed(2)}`;
+
+        const itemremove = document.createElement('button');
+        itemremove.textContent = 'Remove';
+        itemremove.onclick = () => removeFromCart(index);
+
+        listItem.appendChild(img)
+        listItem.appendChild(itemInfo)
+        listItem.appendChild(itemremove)
+
+        cartList.appendChild(listItem);
+    });
 }
+
+function removeFromCart(index) {
+    cart_item.splice(index, 1);
+    updateCartDisplay();
+}
+
