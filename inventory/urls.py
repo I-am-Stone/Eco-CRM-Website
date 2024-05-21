1784658c912f09307cc5a2e5a40830e72ce73bc2
I-am-Stone <dilipchaulagain86@gmail.com>
2024-05-21 16:54:52 +0545

    error solved now working on full functionality

--- inventory/urls.py
+++ inventory/urls.py
@@ -7,7 +7,7 @@ from . import views
 urlpatterns = [
     path("", views.home, name="home"),
     path("add_to_cart", views.add_to_cart, name="add_to_cart"),
-    path("cart", views.cart_details, name="cart"),
+    # path("cart", views.cart_details, name="cart"),
     path("checkout", views.checkout, name="checkout"),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- inventory/views.py
+++ inventory/views.py
@@ -1,65 +1,55 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from inventory.models import *  # Ensure this import matches your actual model location
-
-def home(request):
-    products = ProductInventory.objects.prefetch_related("media_product_inventory").all()
-    
-    context = {
-        'products': products
-    }        
-
-    return render(request, "inventory/cart.html", context)
+from inventory.models import *
+from django.shortcuts import get_object_or_404
 
 def add_to_cart(request):
     if request.method == "POST":
         product_id = request.POST.get('product_id')
         quantity = request.POST.get('quantity')
 
+
+
         cart = request.session.get('cart', {})
-        print(request.session.get('cart', {}))
 
         if product_id in cart:
-            cart[product_id] += int(quantity)
+            cart[product_id] += quantity
         else:
-            cart[product_id] = int(quantity)
-        
+            cart[product_id] = quantity
+
         request.session['cart'] = cart
+        return JsonResponse({"message": "Item added to cart", "cart": cart})
 
-        return JsonResponse({'message': 'Item added to cart successfully'})
-    else:
-        return redirect('home')
-
-def cart_details(request):
+def home(request):
+    products = ProductInventory.objects.prefetch_related("media_product_inventory").all()
+    
+    context = {
+        'products': products
+    }
     cart = request.session.get('cart', {})
     cart_items = []
     total_price = 0
 
-    product_ids = cart.keys()
-    print(product_ids)
-    products = ProductInventory.objects.filter(id__in=product_ids)
+    for product_id, quantity in cart.items():
+            product = ProductInventory.objects.prefetch_related("media_product_inventory").get(pk=product_id)
+            item_total = product.retail_price * quantity
+            total_price += item_total
 
-    print(products)
-
-    for product in products:
-        quantity = cart[str(product.id)]
-        item_total = product.retail_price * quantity
-        total_price += item_total
-
-        cart_items.append({
-            'product_id': product.id,
-            'product_name': product.name,
-            'product_description': product.description,
-            'quantity': quantity,
-            'item_total': item_total,
-        })
+            cart_items.append({
+                'product': product,
+                'quantity': quantity,
+                'item_total': item_total
+            })
 
     context = {
+        'products': products,
         'cart_items': cart_items,
         'total_price': total_price
     }
-    print(context)
-    return render(request, "inventory/cart.html", context)
+    return render(request, "inventory/cart.html",context)
+
+
+
 
 def checkout(request):
     return render(request, "inventory/checkout.html")
