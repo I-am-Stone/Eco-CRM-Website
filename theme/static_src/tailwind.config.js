e983631bf28bad0f865b69dc597a7f314fdaea4e
I-am-Stone <dilipchaulagain86@gmail.com>
2024-07-30 15:59:57 +0545

    finally fixed

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -32,10 +32,6 @@ module.exports = {
         },
     },
     plugins: [
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/line-clamp'),
-        require('@tailwindcss/aspect-ratio'),
         require("daisyui"),
     ],
     daisyui: {
