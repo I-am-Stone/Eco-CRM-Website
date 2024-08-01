26be123af13590640aa2338ee308ca426ada3de1
I-am-Stone <dilipchaulagain86@gmail.com>
2024-08-01 16:29:49 +0545

    fixed

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -114,9 +114,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 INTERNAL_IPS = ["127.0.0.1"]
 
 # NPM binary path for Tailwind
-# NPM_BIN_PATH = " /usr/bin/npm"
+NPM_BIN_PATH = "/home/stone/.nvm/versions/node/v22.4.1/bin/npm"
 
-NPM_BIN_PATH = '/usr/bin/npm'
+# NPM_BIN_PATH = '/usr/bin/npm'
 # Session settings
 SESSION_COOKIE_AGE = 10800  # 3 hours in seconds
 SESSION_SAVE_EVERY_REQUEST = True
