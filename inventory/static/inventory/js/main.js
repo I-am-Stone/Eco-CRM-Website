b5f58b557b98056f9e7e3e5b31f6b2c993498e12
I-am-Stone <dilipchaulagain86@gmail.com>
2024-08-07 11:09:04 +0545

    workign on swiper

--- inventory/static/inventory/js/main.js
+++ inventory/static/inventory/js/main.js
@@ -13,10 +13,28 @@ function closeDrawer() {
 // })
 
 
-function color_change(){
-  document.getElementById('submitted').classList.toggle("step-primary")
-}
 
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
 
 
 
