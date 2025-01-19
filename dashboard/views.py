ccd3af3b2fc6458abeb453cd4c91c257a482f2c5
I-am-Stone <dilipchaulagain86@gmail.com>
2025-01-19 13:04:33 +0545

    small changes

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,9 +5,12 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.shortcuts import redirect
-# from django.views.decorators.csrf import ensure_csrf_cookie
-# from django.views.decorators.http import require_http_methods
-# from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.http import require_http_methods
+import logging
 from django.shortcuts import render, get_object_or_404
 
 from inventory.models import *
@@ -69,7 +72,8 @@ def orders(request):
     }
     return render(request, "dashboard/order.html", context)
 
-
+@csrf_protect
+@require_http_methods(["GET", "POST"])
 def signin(request):
     if request.method == "POST":
         username = request.POST.get('username')
