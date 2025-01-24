9323a63f436135963273be3925c8034e0c900781
I-am-Stone <dilipchaulagain86@gmail.com>
2025-01-24 15:27:57 +0545

    one by one learn more

--- inventory/utils.py
+++
@@ -1,9 +0,0 @@
-from .models import Notification
-
-
-def create_notification(message, notification_type='message', link=None):
-    Notification.objects.create(
-        message=message,
-        notification_type=notification_type,
-        link=link
-    )
